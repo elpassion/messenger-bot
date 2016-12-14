188d77384147e6a084f1b5d45261ebd95382caa1
Urszula Kowalska <ua.kowalska@gmail.com>
2016-12-14 16:43:11 +0100

    Initial commit. This closes #1

---
+++ config/environment.rb
@@ -0,0 +1,40 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/career_bot'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/career_bot_development.sqlite3'
+    #    adapter :sql, 'postgres://localhost/career_bot_development'
+    #    adapter :sql, 'mysql://localhost/career_bot_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/career_bot/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery do
+      development :test
+      test        :test
+      # production :smtp, address: ENV['SMTP_PORT'], port: 1025
+    end
+  end
+end
