f2287c832ee31015b307195b049648ed2cc2715f
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2016-12-27 11:46:38 +0100

    heroku conf changes

--- config/initializers/facebook_messenger.rb
+++ config/initializers/facebook_messenger.rb
@@ -1,9 +1,11 @@
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
+unless Hanami.env == 'test'
+  Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
 
-Facebook::Messenger::Thread.set({
-                                  setting_type: 'call_to_actions',
-                                  thread_state: 'new_thread',
-                                  call_to_actions: [{
-                                    payload: 'WELCOME_PAYLOAD'
-                                  }]
-                                }, access_token: ENV['ACCESS_TOKEN'])
+  Facebook::Messenger::Thread.set({
+                                    setting_type: 'call_to_actions',
+                                    thread_state: 'new_thread',
+                                    call_to_actions: [{
+                                      payload: 'WELCOME_PAYLOAD'
+                                    }]
+                                  }, access_token: ENV['ACCESS_TOKEN'])
+end
