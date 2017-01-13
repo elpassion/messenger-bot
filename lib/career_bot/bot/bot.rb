f92882ac3865acaf16d9b95ba1324faf2628a265
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2017-01-13 14:42:05 +0100

    play_a_game

--- lib/career_bot/bot/bot.rb
+++ lib/career_bot/bot/bot.rb
@@ -17,12 +17,17 @@ Bot.on :postback do |postback|
     when 'WELCOME_PAYLOAD'
       buttons = [
         PostbackButton.new('I wanna find a job!', 'JOB_OFFERS').to_hash,
-        PostbackButton.new('I wanna play a game!', 'TO_BE_DONE').to_hash
+        PostbackButton.new('I wanna play a game!', 'PLAY_A_GAME').to_hash
       ]
       text = 'Hello! :) I am EL Passion Messenger Bot! What would you like to do?'
       postback.reply(attachment: ButtonTemplate.new(text).to_hash(buttons) )
     when 'JOB_OFFERS'
       postback.reply(text: 'Nice! :D Fortunately, we are looking for some cool people to join us! Sooo... What kind of job are you interested in?')
+    when 'PLAY_A_GAME'
+      postback.type
+      sender = postback.sender
+      client = WitConnection.instance.client
+      client.run_actions(sender['id'], 'play_a_game', {})
     when 'TO_BE_DONE'
       postback.reply(text: 'TO BE DONE...')
     else
