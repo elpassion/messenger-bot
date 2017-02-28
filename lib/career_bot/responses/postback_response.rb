2144de6a807a2323f4a61f829fe4af51d50b5add
Urszula Kowalska <ua.kowalska@gmail.com>
2017-02-28 13:45:35 +0100

    Extract bot responses to separate yaml file

--- lib/career_bot/responses/postback_response.rb
+++ lib/career_bot/responses/postback_response.rb
@@ -1,18 +1,9 @@
 class PostbackResponse
   def message(payload)
-    messages[payload.to_sym] || JobDetailsResponse.new(payload).messages
+    response(payload) || JobDetailsResponse.new(payload).messages
   end
 
-  private
-
-  def messages
-    {
-      WELCOME_PAYLOAD: ButtonsResponse.new.welcome_message,
-      JOB_OFFERS: I18n.t('text_messages.job_offers_message'),
-      ABOUT_US: ButtonsResponse.new.about_us_message,
-      WHAT_WE_DO: MediaResponse.new.projects_details_messages,
-      COMPANY: MediaResponse.new.company_details_messages,
-      PEOPLE: I18n.t('text_messages.team_members_message')
-    }
+  def response(payload)
+    I18n.t(payload, locale: :responses) if I18n.exists?(payload, :responses)
   end
 end
