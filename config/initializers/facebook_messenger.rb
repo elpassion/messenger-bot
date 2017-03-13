40df27070723eb47a547a110a9d72794b9a26f49
Urszula Kowalska <ua.kowalska@gmail.com>
2017-03-13 15:15:29 +0100

    Add persistent menu to conversation

--- config/initializers/facebook_messenger.rb
+++ config/initializers/facebook_messenger.rb
@@ -8,4 +8,24 @@ unless Hanami.env == 'test'
                                       payload: 'WELCOME_PAYLOAD'
                                     }]
                                   }, access_token: ENV['ACCESS_TOKEN'])
+
+  Facebook::Messenger::Thread.set({
+                                    setting_type: 'call_to_actions',
+                                    thread_state: 'existing_thread',
+                                    call_to_actions: [{
+                                      type: 'postback',
+                                      title: 'About EL Passion',
+                                      payload: 'ABOUT_US'
+                                    },
+                                    {
+                                      type:'postback',
+                                      title: 'I want to find a job!',
+                                      payload: 'JOB_OFFERS'
+                                    },
+                                    {
+                                      type: 'postback',
+                                      title: 'Help me, please!',
+                                      payload: 'HELP'
+                                    }]
+                                  }, access_token: ENV['ACCESS_TOKEN'])
 end
