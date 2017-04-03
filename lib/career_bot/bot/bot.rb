35e5a33564e6f90101f489003cea35ce864309e3
Urszula Kowalska <ua.kowalska@gmail.com>
2017-04-03 17:43:28 +0200

    Add typing-onn bubble when Bot is processing User message
    Reason:
    Make Bot more responsive

--- lib/career_bot/bot/bot.rb
+++ lib/career_bot/bot/bot.rb
@@ -1,13 +1,23 @@
 include Facebook::Messenger
 
 Bot.on :message do |message|
+  recipient_id = message.sender['id']
+
+  sender_actions_responder.respond(recipient_id, %w(mark_seen typing_on))
   if message.text == 'test'
     message.reply(text: 'OK, I am working! <3')
   else
     MessageResponder.new(message).send
   end
+  sender_actions_responder.respond(recipient_id, 'typing_off')
 end
 
 Bot.on :postback do |postback|
   PostbackResponder.new(postback, PostbackResponse.new.message(postback.payload)).send
+end
+
+private
+
+def sender_actions_responder
+  @sender_actions_responder ||= SenderActionsResponder.new
 end
---
+++ lib/career_bot/responders/sender_actions_responder.rb
@@ -0,0 +1,9 @@
+class SenderActionsResponder
+  def respond(recipient_id, sender_actions)
+    Array(sender_actions).each do |action|
+      Bot.deliver({
+                    recipient: { id: recipient_id },
+                    sender_action: action }, access_token: ENV['ACCESS_TOKEN'])
+    end
+  end
+end
