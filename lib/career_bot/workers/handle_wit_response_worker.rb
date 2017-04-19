c607167bd9e903d576a96900b26b67234b90a597
Urszula Kowalska <ua.kowalska@gmail.com>
2017-04-19 16:25:33 +0200

    Limit Sidekiq retry interval

--- lib/career_bot/workers/handle_wit_response_worker.rb
+++ lib/career_bot/workers/handle_wit_response_worker.rb
@@ -2,6 +2,10 @@ class HandleWitResponseWorker
   include Sidekiq::Worker
   sidekiq_options retry: 3
 
+  sidekiq_retry_in do |count|
+    2 * (count + 1)
+  end
+
   def perform(sender_id, text)
     WitService.new(sender_id, text).send
   end
