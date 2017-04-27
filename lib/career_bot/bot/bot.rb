abc74ebfbd2947922951fa0e7ac814d051f79606
Urszula Kowalska <ua.kowalska@gmail.com>
2017-04-27 14:42:22 +0200

    Add handling messages with gifs/images/stickers

--- lib/career_bot/bot/bot.rb
+++ lib/career_bot/bot/bot.rb
@@ -6,6 +6,9 @@ Bot.on :message do |message|
   SenderActionsResponder.new.respond(recipient_id, %w(mark_seen typing_on))
   if message.text == 'test'
     message.reply(text: 'OK, I am working! <3')
+  elsif message.attachments
+    message.reply(attachment: { type: 'image', payload: { url: GifService.new.random_gif_url } })
+    message.reply(text: 'I love it! <3 Check this out! :D')
   else
     MessageResponder.new(message).set_action
   end
