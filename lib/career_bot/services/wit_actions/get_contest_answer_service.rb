6fd28f77d6b7659dc57efed45dd154dcbd9ce0b1
Urszula Kowalska <ua.kowalska@gmail.com>
2017-06-08 11:57:21 +0200

    Delete unused Wit acitons (connected w/ WDI contest)

--- lib/career_bot/services/wit_actions/get_contest_answer_service.rb
+++
@@ -1,7 +0,0 @@
-class WitAction::GetContestAnswerService < WitAction
-  def call
-    set_context_true 'contest'
-    update_context(context)
-    context
-  end
-end
--- lib/career_bot/services/wit_actions/start_contest_service.rb
+++
@@ -1,7 +0,0 @@
-class WitAction::StartContestService < WitAction
-  def call
-    set_context_true 'contest'
-    update_context(context)
-    context
-  end
-end
