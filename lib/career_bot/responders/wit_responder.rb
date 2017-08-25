0f42cf860596d728025a558ed8cd4f43eb73a0ad
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2017-08-25 11:28:24 +0200

    move show about us from wit.ai, rest of standard stories and remove unused functions form wit_responder

--- lib/career_bot/responders/wit_responder.rb
+++ lib/career_bot/responders/wit_responder.rb
@@ -1,3 +1,4 @@
+# to remove after refactor
 class WitResponder < BotResponder
   def initialize(request, response, **options)
     @request = request
@@ -15,18 +16,6 @@ class WitResponder < BotResponder
 
   def set_response
     case context_key
-    when :job_position
-      found_job_offers
-    when :about_us
-      show_about_us
-    when :offer_details
-      show_details
-    when :main_menu
-      show_main_menu
-    when :details
-      show_details
-    when :gif
-      send_random_gif
     when :error
       send_error_message
     else
@@ -41,28 +30,6 @@ class WitResponder < BotResponder
     bot_deliver(payload)
   end
 
-  def found_job_offers
-    JobOffersResponder.new(session_uid: session_uid,
-                           job_keyword: job_position).response
-  end
-
-  def show_about_us
-    bot_deliver(attachment: I18n.t('ABOUT_US', locale: :responses).first)
-  end
-
-  def show_main_menu
-    bot_deliver(attachment: I18n.t('WELCOME_PAYLOAD', locale: :responses).first)
-  end
-
-  def show_details
-    JobDetailsResponder.new(session_uid: session_uid, details: details).set_response
-  end
-
-  def send_random_gif
-    bot_deliver(attachment: { type: 'image', payload: { url: gif_url } } )
-    bot_deliver(text: I18n.t('RANDOM_GIF', locale: :responses) )
-  end
-
   def add_quick_replies(payload)
     payload[:quick_replies] = response['quickreplies'].map do |reply|
       { content_type: 'text', title: reply, payload: 'empty' }
@@ -74,27 +41,11 @@ class WitResponder < BotResponder
     bot_deliver(text: I18n.t('text_messages.something_went_wrong')[1])
   end
 
-  def job_position
-    @job_position ||= context['job_position']
-  end
-
-  def details
-    @details ||= context['details']
-  end
-
-  def session_uid
-    @session_uid ||= request['session_id']
-  end
-
   def context_key
     context.keys.first.to_sym if context.keys.any?
   end
 
   def context
     @context ||= request['context']
-  end
-
-  def gif_url
-    GifService.new.random_gif_url
   end
 end
